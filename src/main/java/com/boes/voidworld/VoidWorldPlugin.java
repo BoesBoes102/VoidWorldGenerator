60d73daf8b8a9b23f57a639fba5c0a30d37b3ecc
BoesBoes102 <BoesBoes102@gmail.com>
2025-09-12 21:33:33 +0200

    Update

--- src/main/java/com/boes/voidworld/VoidWorldPlugin.java
+++ src/main/java/com/boes/voidworld/VoidWorldPlugin.java
@@ -4,19 +4,26 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
-import org.bukkit.generator.ChunkGenerator;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.TabExecutor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
-public class VoidWorldPlugin extends JavaPlugin {
+public class VoidWorldPlugin extends JavaPlugin implements TabExecutor {
 
     @Override
     public void onEnable() {
         getLogger().info("VoidWorldPlugin enabled.");
+        Objects.requireNonNull(Objects.requireNonNull(this.getCommand("generatevoidworld"))).setExecutor(this);
+        Objects.requireNonNull(this.getCommand("generatevoidworld")).setTabCompleter(this);
     }
 
     @Override
@@ -26,33 +33,49 @@ public class VoidWorldPlugin extends JavaPlugin {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String @NotNull [] args) {
-        if (command.getName().equalsIgnoreCase("generatevoidworld")) {
-            String worldName = "world1";
+        if (!command.getName().equalsIgnoreCase("generatevoidworld")) return false;
 
-            if (Bukkit.getWorld(worldName) != null) {
-                sender.sendMessage(ChatColor.RED + "World '" + worldName + "' already exists!");
-                return true;
-            }
-
-            sender.sendMessage(ChatColor.YELLOW + "Generating void world: " + worldName);
-
-            WorldCreator creator = new WorldCreator(worldName);
-            creator.generator(new ChunkGenerator() {
-                @Override
-                public @NotNull ChunkData generateChunkData(@NotNull World world, @NotNull Random random, int chunkX, int chunkZ, @NotNull BiomeGrid biome) {
-                    return createChunkData(world);
-                }
-            });
-
-            World world = creator.createWorld();
-
-            if (world != null) {
-                sender.sendMessage(ChatColor.GREEN + "Void world '" + worldName + "' created successfully!");
-            } else {
-                sender.sendMessage(ChatColor.RED + "Failed to create the world.");
-            }
+        if (!sender.hasPermission("voidworld.generate")) {
+            sender.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
             return true;
         }
-        return false;
+
+        String worldName = (args.length > 0) ? args[0] : "world1";
+
+        if (Bukkit.getWorld(worldName) != null) {
+            sender.sendMessage(ChatColor.RED + "World '" + worldName + "' already exists!");
+            return true;
+        }
+
+        sender.sendMessage(ChatColor.YELLOW + "Generating void world: " + worldName);
+
+        WorldCreator creator = new WorldCreator(worldName);
+        creator.generator(new ChunkGenerator() {
+            @Override
+            public @NotNull ChunkData generateChunkData(@NotNull World world, @NotNull Random random, int chunkX, int chunkZ, @NotNull BiomeGrid biome) {
+                return createChunkData(world);
+            }
+        });
+
+        World world = creator.createWorld();
+
+        if (world != null) {
+            sender.sendMessage(ChatColor.GREEN + "Void world '" + worldName + "' created successfully!");
+        } else {
+            sender.sendMessage(ChatColor.RED + "Failed to create the world.");
+        }
+
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String @NotNull [] args) {
+        List<String> completions = new ArrayList<>();
+        if (args.length == 1) {
+            completions.add("world1");
+            completions.add("world2");
+            completions.add("world3");
+        }
+        return completions;
     }
 }
